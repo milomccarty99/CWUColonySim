de79326e68ea9d1875983b3b79971d4e1aba44cb
milomccarty99 <milomccarty99@github.com>
2022-10-21 02:03:55 -0700

    added main function

---
+++ CWUColonySim.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <mutex>
+#include <pthread.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "hello world" << endl;
+}
+
+
+class soldier
+{
+
+};
